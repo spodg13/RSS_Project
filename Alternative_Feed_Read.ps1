8dc4043a0316cc4f1fd274f44afb2a965c08a9e8
Darryl Gould <darryl@thegouldhome.com>
2022-01-13 15:51:30 -0800

    Adding Alternative Read

---
+++ Alternative_Feed_Read.ps1
@@ -0,0 +1,8 @@
+﻿$rss.SelectSingleNode('//item')|%{
+    $posts += New-Object psobject -Property @{
+        Title = If($_.Title."#cdata-section"){$_.Title."#cdata-section"}else{$_.Title}
+        Desc = If($_.description."#cdata-section"){$_.description."#cdata-section"}else{$_.Title}
+        link = If($_.link."#cdata-section"){$_.link."#cdata-section"}else{$_.link}
+        pubDate = If($_.pubDate."#cdata-section"){$_.pubDate."#cdata-section"}else{$_.pubDate}
+         }
+     }
