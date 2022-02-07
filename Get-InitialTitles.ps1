1796bf20aec1454b7dd76e7a6d84e488847e7a85
Darryl Gould <35823797+spodg13@users.noreply.github.com>
2022-02-06 17:15:37 -0800

    Minor changes

--- Get-InitialTitles.ps1
+++ Get-InitialTitles.ps1
@@ -7,6 +7,7 @@ Function Get-InitialTitles ($anyarray)
 
     $CleanList =@('a','an','and','as','at','for','from','in','into','is','have', 'has', 'on','of','to','the','with')
     $ti = 0
+    Measure-Command {
     $indexarray = [System.Collections.ArrayList]::new()
     
     foreach($title in $b){
@@ -25,14 +26,14 @@ Function Get-InitialTitles ($anyarray)
             if($count-gt 1) {
                 $i++
                 $total += $count}
-            Write-Host 'Word count: ' $count    
+            #Write-Host 'Word count: ' $count    
         }
-        $title
-        Write-Host 'Index ' $ti
-        Write-Host 'Title - frequent total: '  $total ' Instances +2: ' $i ' Title Key Words: ' $k 
+        #$title
+        #Write-Host 'Index ' $ti
+        #Write-Host 'Title - frequent total: '  $total ' Instances +2: ' $i ' Title Key Words: ' $k 
         if($i -gt 0){$avg = $total/$i}else{$avg=0}
         $hit_ratio = $i/$k
-        Write-Host 'Hit ratio: ' $hit_ratio ' Average hit: ' $avg
+        #Write-Host 'Hit ratio: ' $hit_ratio ' Average hit: ' $avg
         ## if hit_ratio is above .3 (Matching on roughly 3/10 words) or hit $avg above 2 (Some word is triggering)
         ## Capture index number for a scientific check
         if($hit_ratio -ge .25 ) {
@@ -41,6 +42,7 @@ Function Get-InitialTitles ($anyarray)
         $ti ++
     }
     write-host 'Indexes: ' $indexarray 'count ' $indexarray.count
+    }
     return $indexarray
 }
 
